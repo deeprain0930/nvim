3dd09eaffaaf77ff4949fc0e1ab9f368ae909cdf
deeprain0930 <166918016+deeprain0930@users.noreply.github.com>
2024-08-18 14:54:21 +0800

    add cmp

---
+++ lua/deeprain/plugins/cmp.lua
@@ -0,0 +1,96 @@
+local kind_icons = {
+  Text = "󰊄",
+  Method = "m",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "",
+  Variable = "󰫧",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "󰌆",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "󰉺",
+}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
+
+return {
+    "hrsh7th/nvim-cmp",
+    -- event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path", -- source for file system paths
+        "hrsh7th/cmp-cmdline",
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp",
+        },
+        "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim", -- vs-code like pictograms
+    },
+    config = function()
+        local cmp = require("cmp")
+        local lspkind = require("lspkind")
+        local luasnip = require("luasnip")
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.close(),
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                }),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
+            }),
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = function(entry, vim_item)
+                  -- Kind icons
+                  vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+                  -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+                  vim_item.menu = ({
+                    luasnip = "[Snippet]",
+                    buffer = "[Buffer]",
+                    path = "[Path]",
+                  })[entry.source.name]
+                  return vim_item
+                end,
+            }
+        })
+
+        vim.cmd([[
+      set completeopt=menuone,noinsert,noselect
+      highlight! default link CmpItemKind CmpItemMenuDefault
+    ]])
+    end,
+}
